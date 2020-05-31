1f64e0cbebd83cd59209bf5d519ed3ed1efb9606
Damian Wędzikowski <dwedzikowski@gmail.com>
2020-05-31 19:01:55 +0200

    Updates

---
+++ precache-manifest.632befe108a7fb438cb66f7b57bed933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38799460febda6ebd60a1e07c0cc8fe8",
+    "url": "/ReactPagesTest/index.html"
+  },
+  {
+    "revision": "3f7435e4baf8fb5356d2",
+    "url": "/ReactPagesTest/static/js/2.5d19af54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPagesTest/static/js/2.5d19af54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c8e957f120e5919e8c",
+    "url": "/ReactPagesTest/static/js/main.c81ead00.chunk.js"
+  },
+  {
+    "revision": "cd7ca372d5a4d392b993",
+    "url": "/ReactPagesTest/static/js/runtime-main.8f0aa1ea.js"
+  }
+]);
